2ba129ba0655166f560adc045058c45812f9a3ed
Andrés <u1803627@unimilitar.edu.co>
2020-03-12 11:25:33 -0500

    Hola

--- ProyectoAplicaciones/src/proyectoaplicaciones/ProyectoAplicaciones.java
+++ ProyectoAplicaciones/src/proyectoaplicaciones/ProyectoAplicaciones.java
@@ -15,7 +15,8 @@ public class ProyectoAplicaciones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        InterfazGrafica interfaz = new InterfazGrafica();
+        interfaz.setVisible(true);
     }
     
 }
